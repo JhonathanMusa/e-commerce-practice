de08050ee8b4c5e26a70a2c9fd5f311ed0405a92
jhonathanmusa <jhonathansamu91@gmail.com>
2021-02-14 18:52:45 -0500

    adding product details

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,15 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
-import { productListReducer } from "./reducers/productReducers";
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducers";
 
 const initialState = {};
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 // to see redux inside redux devtools
